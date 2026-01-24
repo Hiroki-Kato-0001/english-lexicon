d91649d55ec7a4a37adf290012feb7e4c34b84ef
Hiroki Kato <hiroki.kato0001@gmail.com>
2026-01-24 13:43:46 -0600

    Modified the file name to process

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from get_wikipedia_text import fetch_wikipedia_article
 from detect_region import detect_region_inflection, detect_region_phrase, detect_region_with_spacy
 import json
 
-csv_file = "lexicon_us_uk - raw data.csv"
+csv_file = "lexicon - raw data.csv"
 
 # Generate JSON files from CSV
 result = make_json_file(csv_file)
@@ -35,19 +35,19 @@ sasi_count = 0
 nasi_count = 0
 
 # Load lexicon for inflections from JSON file
-with open('lexicon_us_uk - inflection.json', 'r', encoding='utf-8') as f:
+with open('lexicon - inflection.json', 'r', encoding='utf-8') as f:
     lexicon_inflection = json.load(f)
 
 brit_count, name_count, us_count, can_count, austral_count, nz_count, scot_count, irish_count, ind_count, eafr_count, wafr_count, safr_count, nafr_count, neng_count, easi_count, wasi_count, sasi_count, nasi_count = detect_region_inflection(text, lexicon_inflection, brit_count, name_count, us_count, can_count, austral_count, nz_count, scot_count, irish_count, ind_count, eafr_count, wafr_count, safr_count, nafr_count, neng_count, easi_count, wasi_count, sasi_count, nasi_count)
 
 # Load lexicon for phrases from JSON file
-with open('lexicon_us_uk - phrase.json', 'r', encoding='utf-8') as f:
+with open('lexicon - phrase.json', 'r', encoding='utf-8') as f:
     lexicon_phrase = json.load(f)
 
 brit_count, name_count, us_count, can_count, austral_count, nz_count, scot_count, irish_count, ind_count, eafr_count, wafr_count, safr_count, nafr_count, neng_count, easi_count, wasi_count, sasi_count, nasi_count = detect_region_phrase(text, lexicon_phrase, brit_count, name_count, us_count, can_count, austral_count, nz_count, scot_count, irish_count, ind_count, eafr_count, wafr_count, safr_count, nafr_count, neng_count, easi_count, wasi_count, sasi_count, nasi_count)
 
 # Load lexicon for roots from JSON file
-with open('lexicon_us_uk - root.json', 'r', encoding='utf-8') as f:
+with open('lexicon - root.json', 'r', encoding='utf-8') as f:
     lexicon_root = json.load(f)
 
 result = detect_region_with_spacy(text, lexicon_root, brit_count, name_count, us_count, can_count, austral_count, nz_count, scot_count, irish_count, ind_count, eafr_count, wafr_count, safr_count, nafr_count, neng_count, easi_count, wasi_count, sasi_count, nasi_count)
