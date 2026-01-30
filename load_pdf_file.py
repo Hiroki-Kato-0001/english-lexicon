6a22a7177d20cc3685493e61d1a693d82bc92d11
Hiroki Kato <hiroki.kato0001@gmail.com>
2026-01-29 22:57:37 -0600

    modified to support ocr text

--- load_pdf_file.py
+++ load_pdf_file.py
@@ -1,8 +1,25 @@
 from pypdf import PdfReader
+from pdf2image import convert_from_path
+import pytesseract
 
 def load_text_from_pdf(path):
     reader = PdfReader(path)
     text = ""
+    
     for page in reader.pages:
         text += page.extract_text() + "\n"
+    
+    if text.strip():
+        print("DEBUG: text extracted from PDF using pypdf")
+        return text
+    
+    # Fallback to OCR if text extraction fails
+    print("DEBUG: No text found, running OCR")
+
+    images = convert_from_path(path)
+    text = ""
+
+    for image in images:
+        text += pytesseract.image_to_string(image, lang="eng") + "\n"
+
     return text
