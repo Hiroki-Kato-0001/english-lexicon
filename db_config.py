92393dd7f3054c6ad04384e668717f1ef736b19f
Hiroki Kato <hiroki.kato0001@gmail.com>
2026-03-12 16:59:41 -0600

    installed dotenv

--- db_config.py
+++ db_config.py
@@ -1,9 +1,13 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host="hiroki-A520M-S2H",
-        user="root",
-        password="Database2@25",
-        database="english_lexicon"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
