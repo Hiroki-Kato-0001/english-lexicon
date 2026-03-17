a3c0f698a958d05f4644d0804bf659920641fa62
Hiroki Kato <hiroki.kato0001@gmail.com>
2026-03-17 17:19:13 -0600

    first commit

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
 from import_csv import csv_to_db
+from load_lexicon import load_lexicon
+from get_wikipedia_text import fetch_wikipedia_article
+from load_text_file import load_text_from_txt
+from load_pdf_file import load_text_from_pdf
+from analyse_text import analyze_text, analyze_text_with_spacy
+from visualise import plot_region_bar
 
-def main():
-    csv_file = "lexicon - raw data.csv"
+csv_file = "lexicon - raw data.csv"
 
-    print("Importing CSV into MySQL database...")
+# Import CSV data into MySQL database
+print("Importing CSV into MySQL database...")
 
-    csv_to_db(csv_file)
+csv_to_db(csv_file)
 
-    print("Done.")
+print("Done.")
 
-if __name__ == "__main__":
-    main()
+# Fetch a text from Wikipedia/TXT/PDF
+print("Asking for a Wikipedia URL or a local file path (TXT/PDF) to analyze...")
+link_or_path = input("Enter Wikipedia URL or local file path: ").strip()
+print("User gave input:", link_or_path)
+
+if link_or_path.lower().endswith(".txt"):
+    print("Loading text from TXT file...")
+    text = load_text_from_txt(link_or_path)
+elif link_or_path.lower().endswith(".pdf"):
+    print("Loading text from PDF file...")
+    text = load_text_from_pdf(link_or_path)
+else:
+    print("Fetching text from Wikipedia article...")
+    article = fetch_wikipedia_article(link_or_path)
+    text = article["content"]
+
+text = text.lower()
+print(f"Fetched text: {text[:100]}...")
+
+# Load lexicon data from MySQL database
+print("Loading lexicon data from MySQL database...")
+data = load_lexicon()
+print(f"Loaded {len(data)} lexicon entries.")
+
+# Analyze the text using the lexicon data
+print("Analyzing the text with the inflection and phrase lexicon data...")
+inflection_and_phrase_counts = analyze_text(text, data)
+print("Analysing the text with the root lexicon data")
+final_counts = analyze_text_with_spacy(text, data, *inflection_and_phrase_counts)
+
+# Visualize the results
+print("Visualizing the results...")
+image_path = plot_region_bar(final_counts)
+print(f"Visualization saved to {image_path}")
