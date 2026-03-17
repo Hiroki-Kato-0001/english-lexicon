c64795b191eddfa0b68dcc44e350eabb5cd0e2cd
Hiroki Kato <hiroki.kato0001@gmail.com>
2026-03-17 17:20:12 -0600

    first commit

---
+++ analyse_text.py
@@ -0,0 +1,200 @@
+import re
+import spacy
+import collections
+
+def analyze_text(text, data):
+
+    uk_count = 0
+    name_count = 0
+    us_count = 0
+    can_count = 0
+    austral_count = 0
+    nz_count = 0
+    scot_count = 0
+    irish_count = 0
+    ind_count = 0
+    eafr_count = 0
+    wafr_count = 0
+    safr_count = 0
+    welsh_count = 0
+    neng_count = 0
+    easi_count = 0
+    wasi_count = 0
+    sasi_count = 0
+    seasi_count = 0
+
+    for entry in data:
+
+        if entry['entry_type'] == "Inflection" or entry['entry_type'] == "Phrase":
+            word = entry['word']
+            pattern = rf"\b{re.escape(word)}\b"
+            matches = re.findall(pattern, text)
+            count = len(matches)
+            region = entry['region']
+
+            if count == 0:
+                continue
+
+            if region == "United Kingdom":
+                uk_count += count
+                print(f"matched inflection/phrase United Kingdom:{word} x {count}")
+            if region == "North America":
+                name_count += count
+                print(f"matched inflection/phrase North America:{word} x {count}")
+            if region == "United States":
+                us_count += count
+                print(f"matched inflection/phrase United States:{word} x {count}")
+            if region == "Canada":
+                can_count += count
+                print(f"matched inflection/phrase Canada:{word} x {count}")
+            if region == "Australia":
+                austral_count += count
+                print(f"matched inflection/phrase Australia:{word} x {count}")
+            if region == "New Zealand":
+                nz_count += count
+                print(f"matched inflection/phrase New Zealand:{word} x {count}")
+            if region == "Scotland":
+                scot_count += count
+                print(f"matched inflection/phrase Scotland:{word} x {count}")
+            if region == "Ireland":
+                irish_count += count
+                print(f"matched inflection/phrase Ireland:{word} x {count}")
+            if region == "India":
+                ind_count += count
+                print(f"matched inflection/phrase India:{word} x {count}")
+            if region == "East Africa":
+                eafr_count += count
+                print(f"matched inflection/phrase East Africa:{word} x {count}")
+            if region == "West Africa":
+                wafr_count += count
+                print(f"matched inflection/phrase West Africa:{word} x {count}")
+            if region == "South Africa":
+                safr_count += count
+                print(f"matched inflection/phrase South Africa:{word} x {count}")
+            if region == "Wales":
+                welsh_count += count
+                print(f"matched inflection/phrase Wales:{word} x {count}")
+            if region == "New England":
+                neng_count += count
+                print(f"matched inflection/phrase New England:{word} x {count}")
+            if region == "East Asia":
+                easi_count += count
+                print(f"matched inflection/phrase East Asia:{word} x {count}")
+            if region == "West Asia":
+                wasi_count += count
+                print(f"matched inflection/phrase West Asia:{word} x {count}")
+            if region == "South Asia":
+                sasi_count += count
+                print(f"matched inflection/phrase South Asia:{word} x {count}")
+            if region == "South-East Asia":
+                seasi_count += count
+                print(f"matched inflection/phrase South-East Asia:{word} x {count}")
+    
+    print(f"After analyzing the text, the inflection and phrase counts are: uk_count={uk_count}, name_count={name_count}, us_count={us_count}, can_count={can_count}, austral_count={austral_count}, nz_count={nz_count}, scot_count={scot_count}, irish_count={irish_count}, ind_count={ind_count}, eafr_count={eafr_count}, wafr_count={wafr_count}, safr_count={safr_count}, welsh_count={welsh_count}, neng_count={neng_count}, easi_count={easi_count}, wasi_count={wasi_count}, sasi_count={sasi_count}, seasi_count={seasi_count}")
+    return uk_count, name_count, us_count, can_count, austral_count, nz_count, scot_count, irish_count, ind_count, eafr_count, wafr_count, safr_count, welsh_count, neng_count, easi_count, wasi_count, sasi_count, seasi_count
+
+# Load the spaCy model        
+nlp = spacy.load("en_core_web_sm")
+# Increase the maximum length of text that spaCy can process if needed
+nlp.max_length = 6000000
+
+
+def analyze_text_with_spacy(text, data, uk_count, name_count, us_count, can_count, austral_count,
+                             nz_count, scot_count, irish_count, ind_count, eafr_count, wafr_count, safr_count,
+                             welsh_count, neng_count, easi_count, wasi_count, sasi_count, seasi_count):
+
+    doc = nlp(text)
+    lemmas = [token.lemma_ for token in doc]
+    dict_counter = collections.Counter(lemmas)
+    
+
+    for entry in data:
+
+        if entry['entry_type'] == "Root":
+
+            word = entry['word']
+            count = dict_counter[word]
+            region = entry['region']
+
+            if count == 0:
+                continue
+                
+            if region == "United Kingdom":
+                uk_count += count
+                print(f"matched root United Kingdom:{word} x {count}")
+            if region == "North America":
+                name_count += count
+                print(f"matched root North America:{word} x {count}")
+            if region == "United States":
+                us_count += count
+                print(f"matched root United States:{word} x {count}")
+            if region == "Canada":
+                can_count += count
+                print(f"matched root Canada:{word} x {count}")
+            if region == "Australia":
+                austral_count += count
+                print(f"matched root Australia:{word} x {count}")
+            if region == "New Zealand":
+                nz_count += count
+                print(f"matched root New Zealand:{word} x {count}")
+            if region == "Scotland":
+                scot_count += count
+                print(f"matched root Scotland:{word} x {count}")
+            if region == "Ireland":
+                irish_count += count
+                print(f"matched root Ireland:{word} x {count}")
+            if region == "India":
+                ind_count += count
+                print(f"matched root India:{word} x {count}")
+            if region == "East Africa":
+                eafr_count += count
+                print(f"matched root East Africa:{word} x {count}")
+            if region == "West Africa":
+                wafr_count += count
+                print(f"matched root West Africa:{word} x {count}")
+            if region == "South Africa":
+                safr_count += count
+                print(f"matched root South Africa:{word} x {count}")
+            if region == "Wales":
+                welsh_count += count
+                print(f"matched root Wales:{word} x {count}")
+            if region == "New England":
+                neng_count += count
+                print(f"matched root New England:{word} x {count}")
+            if region == "East Asia":
+                easi_count += count
+                print(f"matched root East Asia:{word} x {count}")
+            if region == "West Asia":
+                wasi_count += count
+                print(f"matched root West Asia:{word} x {count}")
+            if region == "South Asia":
+                sasi_count += count
+                print(f"matched root South Asia:{word} x {count}")
+            if region == "South-East Asia":
+                seasi_count += count
+                print(f"matched root South-East Asia:{word} x {count}")
+
+    print(f"After analyzing the text with spaCy, the final counts are: uk_count={uk_count}, name_count={name_count}, us_count={us_count}, can_count={can_count}, austral_count={austral_count}, nz_count={nz_count}, scot_count={scot_count}, irish_count={irish_count}, ind_count={ind_count}, eafr_count={eafr_count}, wafr_count={wafr_count}, safr_count={safr_count}, welsh_count={welsh_count}, neng_count={neng_count}, easi_count={easi_count}, wasi_count={wasi_count}, sasi_count={sasi_count}, seasi_count={seasi_count}")
+    total = uk_count + name_count + us_count + can_count + austral_count + nz_count + scot_count + irish_count + ind_count + eafr_count + wafr_count + safr_count + welsh_count + neng_count + easi_count + wasi_count + sasi_count + seasi_count
+    
+    return {
+        "uk_count": uk_count,
+        "name_count": name_count,
+        "us_count": us_count,
+        "can_count": can_count,
+        "austral_count": austral_count,
+        "nz_count": nz_count,
+        "scot_count": scot_count,
+        "irish_count": irish_count,
+        "ind_count": ind_count,
+        "eafr_count": eafr_count,
+        "wafr_count": wafr_count,
+        "safr_count": safr_count,
+        "welsh_count": welsh_count,
+        "neng_count": neng_count,
+        "easi_count": easi_count,
+        "wasi_count": wasi_count,
+        "sasi_count": sasi_count,
+        "seasi_count": seasi_count,
+        "total": total
+    }
